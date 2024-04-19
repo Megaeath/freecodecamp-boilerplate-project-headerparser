618336d2ed0a7d62d60ef1239812f0c51843cbc6
nirutk <nirutk@vgi.co.th>
2024-04-19 20:20:34 +0700

    first commit

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ipaddress,
+    language: req.language,
+    software: req.software
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
